fe8de54788e6c5963186efa6750bc06e574380d5
daxapps <daxapps777@gmail.com>
2016-08-11 06:29:15 -0500

    used openweathermap.org API

---
+++ MySunnyApp/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  MySunnyApp
+//
+//  Created by Jason Crawford on 8/11/16.
+//  Copyright © 2016 Jason Crawford. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "a416c187cb84507cd66b60e41f6372a8"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
+
+
+
