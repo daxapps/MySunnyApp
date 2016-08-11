a88c0c33374e69edd1181bb6d2693f8c0921e96c
daxapps <daxapps777@gmail.com>
2016-08-11 04:16:20 -0500

    UI looks good for SE

--- MySunnyApp/WeatherVC.swift
+++ MySunnyApp/WeatherVC.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class WeatherVC: UIViewController {
 
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var currentTempLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLbl: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
